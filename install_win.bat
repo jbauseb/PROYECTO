b58fb9c4d819ed90f77f013300dde56a6a581d0c
José Alfredo Bautista Sebastiao <niper_3@hotmail.com>
2025-04-20 23:28:32 +0200

    Update 20abr25

--- install_win.bat
+++ install_win.bat
@@ -25,19 +25,13 @@ if errorlevel 1 (
     exit /b
 )
 
-REM Paso 1: Clona el repositorio
+REM Clona el repositorio
 echo Clonando el repositorio...
 git clone https://github.com/jbauseb/PROYECTO.git
 
-REM Paso 2: Mueve el proyecto a la carpeta htdocs de XAMPP
+REM Mueve el proyecto a la carpeta htdocs de XAMPP
 echo Copiando archivos a htdocs...
 move /Y PROYECTO "C:\xampp\htdocs"
-
-REM Paso 3: Instala dependencias con Composer
-echo Instalando dependencias con Composer...
-cd /d C:\xampp\htdocs\PROYECTO\Tarea03_4
-composer install
-
 
 REM Iniciar Apache
 echo Iniciando Apache...
@@ -53,11 +47,11 @@ start "" "C:\xampp\mysql_start.bat"
 REM Esperar otros 2 segundos para asegurarse
 timeout /t 2 >nul
 
-REM Paso 5: Crea la base de datos e inserta datos de muestra
+REM Crea la base de datos e inserta datos de muestra
 echo Creando base de datos...
 "C:\xampp\mysql\bin\mysql.exe" -u root < "C:\xampp\htdocs\PROYECTO\Tarea03_4\basedatos\BaseDatos.sql"
 
-REM Paso 6: Abre la aplicación en el navegador
+REM Abre la aplicación en el navegador
 echo Abriendo la aplicacion en el navegador...
 start http://localhost/PROYECTO/Tarea03_4
 
